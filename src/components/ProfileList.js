30ee3db738b206c6956f45059e74b52449eecbfb
Russell Telen <rtelen@my.bcit.ca>
2021-02-24 22:28:18 -0800

    Init table

--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -2,8 +2,34 @@ import React, { useState, useEffect } from "react";
 
 const ProfileList = () => {
   return (
-    <div>
-      <h1>ProfileList</h1>
+    <div className="container">
+      <h1 className="text-center my-3">Profiles</h1>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry the Bird</td>
+            <td>Thornton</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
